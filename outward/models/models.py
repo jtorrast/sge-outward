dc3af4772c685aaf93807a9a1389d8aaaf53a5a6
jtorrast <jordi.torras89@gmail.com>
2023-10-05 15:51:04 +0000

    Modificaciones models player, building

--- outward/models/models.py
+++ outward/models/models.py
@@ -8,7 +8,12 @@ class player(models.Model):
      _description = 'Player'
 
      name = fields.Char()
+     building = fields.Many2one('')#model players_building
      level = fields.Integer()
+     gold = fields.Integer() #computed recorriendo el building para obtener la produccion de gold
+     wood = fields.Integer()
+     stone = fields.Integer()
+
 
 class skill(models.Model):
     _name = 'outward.skill'
@@ -28,3 +33,15 @@ class player_skill_level(models.Model):
 class building(models.Model):
     _name = 'outward.building'
     _description = 'Buildings'
+
+    type = fields.Char()
+    gold_production = fields.Integer()
+
+class player_building(models.Model):
+    _name = 'outward.player_building'
+    _description = 'Player Buildings'
+
+    type = fields.Many2one('outward.building')
+    player = fields.Many2one('outward.player')
+    quantity = fields.Integer()
+
