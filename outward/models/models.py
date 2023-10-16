8f308317eb85723b81302a21ec2e11d7525c8e7c
jtorrast <jordi.torras89@gmail.com>
2023-10-16 13:36:36 +0000

    Cambio modelos edificios y obtener produccion

--- outward/models/models.py
+++ outward/models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 
 from odoo import models, fields, api
 
@@ -8,11 +9,17 @@ class player(models.Model):
      _description = 'Player'
 
      name = fields.Char()
-     building = fields.Many2one('')#model players_building
+     building = fields.One2Many('outward.player_building', 'player')#model players_building
      level = fields.Integer()
-     gold = fields.Integer() #computed recorriendo el building para obtener la produccion de gold
+     gold = fields.Integer(compute='_get_resource') #computed recorriendo el building para obtener la produccion de gold
      wood = fields.Integer()
      stone = fields.Integer()
+     colonist = fields.Integer()
+
+     @api.depends('building')
+     def _get_resouce(self):
+         for b in self:
+             b.gold = b.building.gold_production
 
 
 class skill(models.Model):
@@ -30,12 +37,20 @@ class player_skill_level(models.Model):
     skill_id = fields.Many2one('outward.skill')
     skill_level = fields.Integer()
 
+
+
 class building(models.Model):
     _name = 'outward.building'
     _description = 'Buildings'
 
     type = fields.Char()
+    food_production = fields.Integer()
+    wood_production = fields.Integer()
+    stone_production = fields.Integer()
     gold_production = fields.Integer()
+    soldier_production = fields.Integer()
+    colonist_production = fields.Integer()
+
 
 class player_building(models.Model):
     _name = 'outward.player_building'
@@ -43,5 +58,21 @@ class player_building(models.Model):
 
     type = fields.Many2one('outward.building')
     player = fields.Many2one('outward.player')
-    quantity = fields.Integer()
+    building_level = fields.Integer(default=1)
+    food_production = fields.Integer(compute='_get_production')
+    wood_production = fields.Integer(compute='_get_production')
+    stone_production = fields.Integer(compute='_get_production')
+    gold_production = fields.Integer(compute='_get_production')
+    soldier_production = fields.Integer(compute='_get_production')
+    colonist_production = fields.Integer(compute='_get_production')
+
+    @api.depends('type', 'building_level')
+    def _get_prodution(self):
+        for b in self:
+            b.food_production = b.food_production + b.type.food_production * math.log(b.building_level)
+            b.wood_production = b.wood_production + b.type.wood_production * math.log(b.building_level)
+            b.stone_production = b.stone_production + b.type.stone_production * math.log(b.building_level)
+            b.gold_production = b.gold_production + b.type.gold_production * math.log(b.building_level)
+            b.soldier_production = b.soldier_production + b.type.soldier_production * math.log(b.building_level)
+            b.colonist_production = b.colonist_production + b.type.colonist_production * math.log(b.building_level)
 
