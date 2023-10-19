1f69ea0dd5b5fddfc806c6c859bd5b19d391c024
jtorrast <jordi.torras89@gmail.com>
2023-10-19 15:43:38 +0000

    Correcto carga recursos pj

--- outward/models/models.py
+++ outward/models/models.py
@@ -2,6 +2,7 @@
 import math
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class player(models.Model):
@@ -13,16 +14,19 @@ class player(models.Model):
      level = fields.Integer()
      gold = fields.Integer(compute='_get_resource') #computed recorriendo el building para obtener la produccion de gold
      wood = fields.Integer(compute='_get_resource')
+     food = fields.Integer(compute='_get_resource')
      stone = fields.Integer(compute='_get_resource')
      colonist = fields.Integer(compute='_get_resource')
 
      @api.depends('building')
      def _get_resource(self):
-         for b in self:
-             b.gold = b.building.gold_production
-             b.wood = b.building.wood_production
-             b.stone = b.building.stone_production
-             b.colonist = b.building.colonist_production
+         for player in self:
+             for building in self.building:
+                 player.gold += building.gold_production
+                 player.wood += building.wood_production
+                 player.stone += building.stone_production
+                 player.colonist += building.colonist_production
+                 player.food += building.food_production
 
 
 class skill(models.Model):
@@ -40,8 +44,6 @@ class player_skill_level(models.Model):
     skill_id = fields.Many2one('outward.skill')
     skill_level = fields.Integer()
 
-
-
 class building(models.Model):
     _name = 'outward.building'
     _description = 'Buildings'
@@ -51,8 +53,15 @@ class building(models.Model):
     wood_production = fields.Integer()
     stone_production = fields.Integer()
     gold_production = fields.Integer()
-    soldier_production = fields.Integer()
+    soldier_production = fields.Integer()#ver de hacer seccion modelos para militares y desblquean según edificios
     colonist_production = fields.Integer()
+    food_cost = fields.Integer()
+    wood_cost = fields.Integer()
+    stone_cost = fields.Integer()
+    gold_cost = fields.Integer()
+
+
+
 
 
 class player_building(models.Model):
@@ -61,6 +70,7 @@ class player_building(models.Model):
 
     type = fields.Many2one('outward.building')
     player = fields.Many2one('outward.player')
+    #campo nombre con constrais para probar que no sea repetido
     building_level = fields.Integer(default=1)
     food_production = fields.Integer(compute='_get_production')
     wood_production = fields.Integer(compute='_get_production')
@@ -69,7 +79,7 @@ class player_building(models.Model):
     soldier_production = fields.Integer(compute='_get_production')
     colonist_production = fields.Integer(compute='_get_production')
 
-    @api.depends('type', 'building_level')
+    @api.depends('type')
     def _get_production(self):
         for b in self:
             b.food_production = b.type.food_production + b.type.food_production * math.log(b.building_level)
@@ -78,5 +88,4 @@ class player_building(models.Model):
             b.gold_production = b.type.gold_production + b.type.gold_production * math.log(b.building_level)
             b.soldier_production = b.type.soldier_production + b.type.soldier_production * math.log(b.building_level)
             b.colonist_production = b.type.colonist_production + b.type.colonist_production * math.log(b.building_level)
-
 
