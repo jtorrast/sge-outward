c2b0e5e4799b570597494e69522956960241371c
jtorrast <jordi.torras89@gmail.com>
2023-12-18 04:35:17 +0000

    Sistema de mercenarios, cron, botones y vista

--- outward/models/models.py
+++ outward/models/models.py
@@ -5,75 +5,93 @@ from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
 
+# ctrl + alt + L -> dar formato al código
 
 class player(models.Model):
-     _name = 'outward.player'
-     _description = 'Player'
+    _name = 'outward.player'
+    _description = 'Player'
 
-     name = fields.Char()
-     building = fields.One2many('outward.player_building', 'player')#model players_building
-     level = fields.Integer()
-     gold = fields.Integer() #computed recorriendo el building para obtener la produccion de gold
-     wood = fields.Integer()
-     food = fields.Integer()
-     stone = fields.Integer()
-     colonist = fields.Integer()
-     available_buildings = fields.Many2many('outward.building', compute='_get_available_buildings')
+    name = fields.Char()
+    building = fields.One2many('outward.player_building', 'player')  # model players_building
+    level = fields.Integer()
+    gold = fields.Integer()  # computed recorriendo el building para obtener la produccion de gold
+    wood = fields.Integer()
+    food = fields.Integer()
+    stone = fields.Integer()
+    colonist = fields.Integer()
+    attack = fields.Integer(compute='_get_militia_stats', store=True)
+    defence = fields.Integer(compute='_get_militia_stats', store=True)
+    available_buildings = fields.Many2many('outward.building', compute='_get_available_buildings')
+    militia = fields.One2many('outward.player_militia', 'player')
+    available_militia = fields.Many2many('outward.militia_type', compute='_get_available_militia')
 
-     def _get_available_buildings(self):
-         for c in self:
-             print(c)
-             c.available_buildings = self.env['outward.building'].search([]).filtered(lambda b: b.wood_cost <= c.wood and
-                                                                                                b.stone_cost <= c.stone).ids
+    @api.depends('militia.attack', 'militia.defence', 'militia.level')
+    def _get_militia_stats(self):
+        for player in self:
+            active_militia = player.militia.filtered(lambda m: m.level > 0)
+            player.attack = sum(active_militia.mapped('attack'))
+            player.defence = sum(active_militia.mapped('defence'))
 
-     def generate_colonist(self):
-         #para comprobar edificio recorrer con un for los edificios y camviar una variable bool a true si lo encuentra
+    def _get_available_buildings(self):
+        for c in self:
+            print(c)
+            c.available_buildings = self.env['outward.building'].search([]).filtered(lambda b: b.wood_cost <= c.wood and
+                                                                                               b.stone_cost <= c.stone).ids
 
-         for p in self:
-             for building in p.building:
-                 have_barrack = False
-                 print(building.name)
-                 if building.name == 'Barrack':
-                     have_barrack = True
-                     print("Tienes BARRACK")
-                     break
-             if not have_barrack:
-                 raise ValidationError("You need some Barracks")
+    def _get_available_militia(self):
+        for b in self:
+            b.available_militia = self.env['outward.militia_type'].search([]).filtered(lambda c: c.gold_cost <= b.gold).ids
 
-             if p.food >= 50 and have_barrack:
-                 p.colonist = p.colonist + 1
-                 print("Colonist +1", p.colonist)
-             else:
-                 raise ValidationError("You don't have enough food")
 
-     @api.depends('building')
-     def _get_resource(self):
-         for player in self:
-             player.gold = 0
-             player.wood = 0
-             player.stone = 0
-             player.food = 0
-             for building in player.building:
-                 player.gold += building.gold_production
-                 player.wood += building.wood_production
-                 player.stone += building.stone_production
-                 player.food += building.food_production
 
-     def update_resources(self):
-         for player in self.search([]):
-             gold = player.gold
-             wood = player.wood
-             stone = player.stone
-             food = player.food
-             for building in player.building:
-                 gold += building.gold_production
-                 wood += building.wood_production
-                 stone += building.stone_production
-                 food += building.food_production
-             player.gold = gold
-             player.wood = wood
-             player.stone = stone
-             player.food = food
+    def generate_colonist(self):
+        # para comprobar edificio recorrer con un for los edificios y camviar una variable bool a true si lo encuentra
+
+        for p in self:
+            for building in p.building:
+                have_barrack = False
+                print(building.name)
+                if building.name == 'Barrack':
+                    have_barrack = True
+                    print("Tienes BARRACK")
+                    break
+            if not have_barrack:
+                raise ValidationError("You need some Barracks")
+
+            if p.food >= 50 and have_barrack:
+                p.colonist = p.colonist + 1
+                print("Colonist +1", p.colonist)
+            else:
+                raise ValidationError("You don't have enough food")
+
+    @api.depends('building')
+    def _get_resource(self):
+        for player in self:
+            player.gold = 0
+            player.wood = 0
+            player.stone = 0
+            player.food = 0
+            for building in player.building:
+                player.gold += building.gold_production
+                player.wood += building.wood_production
+                player.stone += building.stone_production
+                player.food += building.food_production
+
+    def update_resources(self):
+        for player in self.search([]):
+            gold = player.gold
+            wood = player.wood
+            stone = player.stone
+            food = player.food
+            for building in player.building:
+                gold += building.gold_production
+                wood += building.wood_production
+                stone += building.stone_production
+                food += building.food_production
+            player.gold = gold
+            player.wood = wood
+            player.stone = stone
+            player.food = food
 
 
 class skill(models.Model):
@@ -83,6 +101,7 @@ class skill(models.Model):
     name = fields.Char()
     description = fields.Char()
 
+
 class player_skill_level(models.Model):
     _name = 'outward.player_skill_level'
     _description = 'Player Skills'
@@ -91,13 +110,14 @@ class player_skill_level(models.Model):
     skill_id = fields.Many2one('outward.skill')
     skill_level = fields.Integer()
 
+
 class building(models.Model):
     _name = 'outward.building'
     _description = 'Buildings'
 
     type = fields.Char()
     name = fields.Char(compute='_get_name')
-    #comprobar si el selection se muestra en la vista
+    # comprobar si el selection se muestra en la vista
     food_production = fields.Integer()
     wood_production = fields.Integer()
     stone_production = fields.Integer()
@@ -130,7 +150,6 @@ class building(models.Model):
             building.player.wood -= building.wood_cost
 
 
-
 class player_building(models.Model):
     _name = 'outward.player_building'
     _description = 'Player Buildings'
@@ -140,7 +159,6 @@ class player_building(models.Model):
     name = fields.Char(compute='_get_name')
     estate = fields.Selection([('Construcción', 'Construcción'), ('Operativo', 'Operativo'), ('Mejora', 'Mejora')],
                               required=True, default='Construcción')
-    #campo nombre con constrais para probar que no sea repetido
     building_level = fields.Integer(default=1)
     food_production = fields.Integer(compute='_get_production')
     wood_production = fields.Integer(compute='_get_production')
@@ -157,12 +175,14 @@ class player_building(models.Model):
     @api.depends('type')
     def _get_production(self):
         for b in self:
-            b.food_production = b.type.food_production + b.type.food_production * math.log(b.building_level)
-            b.wood_production = b.type.wood_production + b.type.wood_production * math.log(b.building_level)
-            b.stone_production = b.type.stone_production + b.type.stone_production * math.log(b.building_level)
-            b.gold_production = b.type.gold_production + b.type.gold_production * math.log(b.building_level)
-            b.soldier_production = b.type.soldier_production + b.type.soldier_production * math.log(b.building_level)
-            b.colonist_production = b.type.colonist_production + b.type.colonist_production * math.log(b.building_level)
+            b.food_production = b.type.food_production + b.type.food_production * math.log(b.building_level + 1)
+            b.wood_production = b.type.wood_production + b.type.wood_production * math.log(b.building_level + 1)
+            b.stone_production = b.type.stone_production + b.type.stone_production * math.log(b.building_level + 1)
+            b.gold_production = b.type.gold_production + b.type.gold_production * math.log(b.building_level + 1)
+            b.soldier_production = b.type.soldier_production + b.type.soldier_production * math.log(
+                b.building_level + 1)
+            b.colonist_production = b.type.colonist_production + b.type.colonist_production * math.log(
+                b.building_level + 1)
 
     # def cron_building_construcion(self):
     #   for b in self([]):
@@ -171,9 +191,72 @@ class player_building(models.Model):
     # Si cambiamos a estado mejora sube de nivel si tenemos los recursos necesarios para la subida
 
 
-#pensar sistemas de defensa
+class militia_type(models.Model):
+    _name = 'outward.militia_type'
+    _description = 'Militia'
 
-#modelo soldados y soldados_jugador:
-#guard -> barrack. Coste 60 food, 20 gold, 20 seg
-#cowboy-> establo, food 25, gold 45, 25 seg
-#marshals-> fuerte, food 60, gold 75, 30 seg
+    name = fields.Char()
+    attack = fields.Float()
+    defence = fields.Float()
+    gold_cost = fields.Float()
+
+    def hire(self):
+        for b in self:
+            player_id = self._context.get('player_id')
+            militia = self.env['outward.player_militia'].create({
+                "type": b.id,
+                "player": player_id
+            })
+            militia.player.gold -= militia.gold_cost
+
+
+class player_militia(models.Model):
+    _name = 'outward.player_militia'
+    _description = 'Players Militia'
+
+    name = fields.Char(compute='get_name_militia')
+    type = fields.Many2one('outward.militia_type')
+    player = fields.Many2one('outward.player')
+    level = fields.Integer(default=0)
+    hire_percent = fields.Float(default=0)
+    attack = fields.Float(compute='_get_stats')
+    defence = fields.Float(compute='_get_stats')
+    gold_cost = fields.Float(compute='_get_stats')
+    is_active = fields.Boolean(compute='_get_is_active')
+
+    @api.depends('player')
+    def _get_is_active(self):
+        for b in self:
+            b.is_active = True
+            if b.attack < 0 and b.player.attack <= abs(b.attack):
+                b.is_active = False
+            if b.defence < 0 and b.player.defence <= abs(b.defence):
+                b.is_active = False
+    @api.depends('type', 'player')
+    def get_name_militia(self):
+        for b in self:
+            b.name = 'undefined'
+            if b.type and b.player:
+                b.name = b.type.name + " " + b.player.name + " " + str(b.id)
+
+    @api.depends('type')
+    def _get_stats(self):
+        for b in self:
+            b.attack = b.type.attack + b.type.attack * math.log(b.level + 1)
+            b.defence = b.type.defence + b.type.defence * math.log(b.level + 1)
+            b.gold_cost = b.type.gold_cost * b.level
+
+    def hire_level(self):
+        for b in self.search([('hire_percent','<',100)]):
+            b.hire_percent += 1/(b.level+1)
+            if(b.hire_percent >= 100):
+                b.hire_percent = 100
+                b.level += 1
+            print(b.name,b.hire_percent)
+
+# pensar sistemas de defensa
+
+# modelo soldados y soldados_jugador:
+# guard -> barrack. Coste 60 food, 20 gold, 20 seg
+# cowboy-> establo, food 25, gold 45, 25 seg
+# marshals-> fuerte, food 60, gold 75, 30 seg
